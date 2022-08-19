8c1d35115293e124a2d959226613173fb597f6d9
koval <88kg@mail.ru>
2022-08-19 22:01:41 +0300

    делаю отправку изображения

--- helpers.go
+++ helpers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func IntToStr(number int) string {
@@ -10,4 +11,22 @@ func IntToStr(number int) string {
 
 func FloatToStr(number float64) string {
 	return fmt.Sprintf("%.2f", number)
+}
+
+func findMinAndMax(a []float64) (min float64, max float64) {
+	min = a[0]
+	max = a[0]
+	for _, value := range a {
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
+		}
+	}
+	return min, max
+}
+
+func random(min, max float64) float64 {
+	return rand.Float64()*(max-min) + min
 }
